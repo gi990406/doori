5cc9648568390d5dbb034533bbc0963ad64e0764
gi990406 <rlshdnr4550@nate.com>
2025-07-16 19:24:57 +0900

    chore: media setting

--- doori/settings.py
+++ doori/settings.py
@@ -138,6 +138,9 @@ STATICFILES_DIRS =[
 STATIC_ROOT =BASE_DIR / "staticfiles"
 # STATIC_ROOT = os.path.join("staticfiles")
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
