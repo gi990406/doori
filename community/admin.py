f3f657f30369dcb4d140989f97f0975bc62c93ba
gi990406 <rlshdnr4550@nate.com>
2025-07-23 14:14:10 +0900

    feat: notice admin register

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . import models
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+class ImageInline(admin.TabularInline):
+    model = models.Notice_Image
+    extra = 1
+
+@admin.register(models.Notice)
+class NoticeAdmin(SummernoteModelAdmin):
+    inlines = [ImageInline]
+    summernote_fields = ['content',]
